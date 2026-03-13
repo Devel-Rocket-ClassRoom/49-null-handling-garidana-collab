698b777e3e01020d2ce7fed7396694a3b2b5341e
garidana-collab <garidana@gmail.com>
2026-03-13 23:30:24 +0900

    49 NullableScoreBoard finished

--- NullableScoreBoard/Program.cs
+++ NullableScoreBoard/Program.cs
@@ -1,4 +1,46 @@
 using System;
-
+using static System.Console;
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+int?[] scores = { 3, null, 7, null, 5 };
+int N = 0;
+int totalGames = 0;
+int? totalScore = 0;
+WriteLine("=== 경기 상태 ===");
+
+foreach (int? score in scores)
+{
+    N++;
+    if (score.HasValue)
+    {
+        WriteLine($"경기 {N}: {score}점 (진행 완료)");
+        totalGames++;
+        totalScore += score;
+    }
+    else
+    {
+        WriteLine($"경기 {N}: 미진행");
+    }
+
+}
+N = 0;
+WriteLine();
+WriteLine("=== 전체 점수 (미진행 = -1) ===");
+foreach (int? score in scores)
+{
+    N++;
+    if (score.HasValue)
+    {
+        WriteLine($"경기 {N}: {score.GetValueOrDefault(-1)}");
+        
+    }
+    else
+    {
+        WriteLine($"경기 {N}: {score.GetValueOrDefault(-1)}");
+    }
+}
+N = 0;
+WriteLine();
+WriteLine("=== 통계 ===");
+WriteLine($"진행된 경기 수: {totalGames}");
+WriteLine($"총점: {totalScore}");
+WriteLine($"평균: {totalScore / totalGames:N1}");
