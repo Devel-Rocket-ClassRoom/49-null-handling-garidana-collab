7ca6fbcaa40c329ecf830734dcef115e51c8172c
garidana-collab <garidana@gmail.com>
2026-03-13 17:52:41 +0900

    49 Coding Practice 2 inprogress

--- CodingPractice-02/Program.cs
+++ CodingPractice-02/Program.cs
@@ -1,5 +1,32 @@
 using System;
 using System.Collections.Generic;
-
+using static System.Console;
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+// 1-1
+string name = null;
+name ??= "이름 없어";
+WriteLine(name);
+name ??= "뉴 네임";
+WriteLine(name);
+
+WriteLine();
+
+// 1-2
+string playerName = null;
+if (playerName == null)
+{
+    playerName = "Player";
+}
+WriteLine(playerName);
+
+WriteLine();
+
+// 1-3
+
+List<string> _items = null;
+_items ??= new List<string>();
+_items.Add("검");
+
+_items = new List<string>();
+_items.Add("방패");
